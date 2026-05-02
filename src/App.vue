359cdd832242126e7329fb270f783f7f41ada205
SRIRANAA <innamuriranaaprathap@gmail.com>
2026-05-02 20:12:15 +0530

    Initial commit: Employee Management System

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="bg-dark text-white text-center py-4 mb-4">
+      <h1>Employee Management System</h1>
+      <h5 class="text-secondary">CRUD Operations using Vue.js, Axios and MockAPI</h5>
+    </div>
+
+    <EmployeeForm />
+    <EmployeeList />
+    <UpdateEmployee />
+    <DeleteEmployee />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeForm from './components/EmployeeForm.vue';
+import EmployeeList from './components/EmployeeList.vue';
+import UpdateEmployee from './components/UpdateEmployee.vue';
+import DeleteEmployee from './components/DeleteEmployee.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeForm,
+    EmployeeList,
+    UpdateEmployee,
+    DeleteEmployee
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container my-4">
+    <div class="card shadow-sm">
+      <div class="card-header bg-success text-white">
+        <h4 class="mb-0">Employee List (axios GET)</h4>
+      </div>
+      <div class="card-body">
+        <table class="table table-bordered table-hover">
+          <thead class="table-success">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Designation</th>
+              <th>Department</th>
+              <th>Salary</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in list" :key="item.id">
+              <td>{{ item.id }}</td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.designation }}</td>
+              <td>{{ item.department }}</td>
+              <td>{{ item.salary }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "EmployeeList",
+  data() {
+    return {
+      list: []
+    }
+  },
+  mounted() {
+    axios.get('https://69f5fe38a72f01a951b915f3.mockapi.io/EmployeeData')
+      .then((resp) => {
+        this.list = resp.data;
+      })
+      .catch(err => console.error(err));
+  }
+}
+</script>
